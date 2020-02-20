6843062dcce4a4e9786c33988f000983b193378b
pedrolmr <pedroluismontesinosrivera@gmail.com>
2020-02-19 23:47:06 -0500

    added localstorage

--- Movie-seat-booking/script.js
+++ Movie-seat-booking/script.js
@@ -4,10 +4,20 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
+populateUI();
+
 let ticketPrice = +movieSelect.value; //converts string into a number
+
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem('selectedMovieIndex', movieIndex);
+  localStorage.setItem('selectedMoviePrice', moviePrice);
+}
 
 function updateSelectedCount() {
   const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+  const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
 
   const selectedSeatsCount = selectedSeats.length;
 
@@ -15,8 +25,28 @@ function updateSelectedCount() {
   total.innerText = selectedSeatsCount * ticketPrice;
 }
 
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add('selected');
+      }
+    });
+  }
+
+  const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+  if (selectedMovieIndex !== null) {
+    movieSelect.selectedIndex = selectedMovieIndex;
+  }
+}
+
 movieSelect.addEventListener('change', e => {
   ticketPrice = +e.target.value;
+
+  setMovieData(e.target.selectedIndex, e.target.value);
+
   updateSelectedCount();
 });
 
@@ -30,3 +60,5 @@ container.addEventListener('click', e => {
     updateSelectedCount();
   }
 });
+
+updateSelectedCount();
