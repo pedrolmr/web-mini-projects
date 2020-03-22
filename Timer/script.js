4c43bd7e350a2c2c782b294c060e16867df81a45
pedrolmr <pedroluismontesinosrivera@gmail.com>
2020-03-21 23:42:26 -0400

    timer project

---
+++ Timer/script.js
@@ -0,0 +1,20 @@
+const durationInput = document.querySelector('#duration');
+const startButton = document.querySelector('#start');
+const pauseButton = document.querySelector('#pause');
+
+class Timer {
+  constructor(durationInput, startButton, pauseButton) {
+    this.durationInput = durationInput;
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
+
+    this.startButton.addEventListener('click', this.start);
+  }
+
+  start() {
+    console.log(this);
+  }
+}
+
+const timer = new Timer(durationInput, startButton, pauseButton);
+timer.start();
