2203aecbd5f5b21ccca98606ebf296202ea12282
pedrolmr <pedroluismontesinosrivera@gmail.com>
2020-04-15 01:21:21 -0400

    added css

--- netflix/gulpfile.js
+++ netflix/gulpfile.js
@@ -3,17 +3,21 @@ const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 
 function css() {
-    return gulp
-        .src('scss/app.scss')
-        .pipe(autoprefixer() )
-        .pipe(sass({ outputStyle: "expanded" }))
-        .pipe(gulp.dest("css"))
-}
-function watchFiles() {
-    gulp.watch('scss/*.scss', css);
-    gulp.watch('index.html')
+  return gulp
+    .src('scss/app.scss')
+    .pipe(
+      autoprefixer({
+        overrideBrowserslist: ['last 2 versions'],
+      })
+    )
+    .pipe(sass({ outputStyle: 'expanded' }))
+    .pipe(gulp.dest('css'));
 }
 
-// tasks
+function watchFiles() {
+  gulp.watch('scss/*.scss', css);
+  gulp.watch('index.html');
+}
+
 gulp.task('css', css);
-gulp.task("watch", gulp.parallel(watchFiles));
+gulp.task('watch', gulp.parallel(watchFiles));
